a2bd859543f4ddbb6eb571e3df9470743260cba7
zydan25 <zydanalattab25w@gmail.com>
2026-08-22 17:57:21 +0300

    admin: expose per-order customer chats

--- backend/marketplace/admin_marketplace.py
+++ backend/marketplace/admin_marketplace.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 
 from .marketplace_models import CouponRedemption, InventoryReservation, Payment, Shipment, VendorApplication, VendorLedgerEntry, VendorOrder, VendorOrderItem
 from .models import VendorProfile
+from .order_chat_models import OrderChat, OrderChatMessage
 from .storefront_models import StorefrontMedia
 
 
@@ -91,3 +92,20 @@ class CouponRedemptionAdmin(admin.ModelAdmin):
     list_filter = ("currency", "created_at")
     search_fields = ("code_snapshot", "coupon__code", "user__phone", "order__order_number")
     readonly_fields = tuple(field.name for field in CouponRedemption._meta.fields)
+
+
+@admin.register(OrderChat)
+class OrderChatAdmin(admin.ModelAdmin):
+    list_display = ("order", "vendor", "customer", "is_closed", "updated_at")
+    list_filter = ("is_closed", "vendor")
+    search_fields = ("order__order_number", "vendor__store_name", "customer__phone")
+    readonly_fields = ("order", "vendor_order", "vendor", "customer", "created_at", "updated_at")
+    ordering = ("-updated_at",)
+
+
+@admin.register(OrderChatMessage)
+class OrderChatMessageAdmin(admin.ModelAdmin):
+    list_display = ("chat", "sender", "is_read", "created_at")
+    list_filter = ("is_read", "created_at")
+    search_fields = ("chat__order__order_number", "sender__phone", "body")
+    readonly_fields = ("chat", "sender", "created_at")
