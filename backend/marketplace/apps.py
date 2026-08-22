48acf540f49a7852e83bfe5864dce1207effcf36
zydan25 <zydanalattab25w@gmail.com>
2026-08-22 17:40:07 +0300

    django: register split marketplace model modules during app startup

--- backend/marketplace/apps.py
+++ backend/marketplace/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class MarketplaceConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "marketplace"
+
+    def ready(self):
+        from . import marketplace_models  # noqa: F401
+        from . import storefront_models  # noqa: F401
