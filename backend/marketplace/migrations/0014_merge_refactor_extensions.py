e417b9c941ca368cce1928cb1b95b85b85332f95
zydan25 <zydanalattab25w@gmail.com>
2026-08-26 20:55:32 +0300

    Merge marketplace extension migrations into refactor chain

---
+++ backend/marketplace/migrations/0014_merge_refactor_extensions.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("marketplace", "0013_merge_storefront_and_order_chat"),
+        ("marketplace", "0006_platform_ledger"),
+    ]
+    operations = []
