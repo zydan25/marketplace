75a2eba5ab758fdd0126bdf3a7b2839540720615
zydan25 <zydanalattab25w@gmail.com>
2026-08-28 17:44:10 +0300

    Merge parallel marketplace migration branches

---
+++ backend/marketplace/migrations/0014_merge_catalog_and_storefront.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("marketplace", "0011_catalog_currency_shipping"),
+        ("marketplace", "0013_merge_storefront_and_order_chat"),
+    ]
+
+    operations = []
