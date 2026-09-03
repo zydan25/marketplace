23c7b852bd936874147733eebf3896f06265643d
zydan25 <zydanalattab25w@gmail.com>
2026-09-03 21:17:59 +0300

    chore: merge legacy marketplace migration leaf with separation history

---
+++ backend/marketplace/migrations/0019_merge_domain_separation_history.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("marketplace", "0018_complete_domain_separation"),
+        ("marketplace", "0013_merge_storefront_migrations"),
+    ]
+
+    operations = []
