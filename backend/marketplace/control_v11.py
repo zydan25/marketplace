c479fc946bfd70ca89222b45e4054798b02036b7
zydan25 <zydanalattab25w@gmail.com>
2026-09-17 22:16:32 +0300

    fix(control): wrap direct control routes in ERP shell

--- backend/marketplace/control_v11.py
+++ backend/marketplace/control_v11.py
@@ -1,8 +1,10 @@
 import json
+import re
 
 from django.contrib import messages
 from django.core.paginator import Paginator
 from django.db.models import Sum
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from django.views.decorators.http import require_http_methods
@@ -47,8 +49,6 @@ def _product_editor_context(form, product=None):
 @dashboard_access_required
 @require_http_methods(["GET"])
 def product_detail(request, product_id):
-    from django.shortcuts import get_object_or_404
-    from catalog.models import Product
     product = get_object_or_404(
         Product.objects.select_related("vendor", "vendor__owner").prefetch_related("categories", "store_categories", "variants", "image_items"),
         pk=product_id,
@@ -91,21 +91,55 @@ def application_review(request, application_id):
 
 legacy._product_editor_context = _product_editor_context
 legacy.product_detail = product_detail
-legacy.store_detail = core.store_detail
 core.product_detail = product_detail
 core._product_editor_context = _product_editor_context
 
-control_products = legacy.control_products
-control_stores = legacy.control_stores
-control_categories = core.control_categories
-control_inventory = core.control_inventory
-control_orders = core.control_orders
-order_detail = core.order_detail
+
+def _wrap_control_response(request, response):
+    """Return control fragments for AJAX, but a complete responsive ERP shell for direct page loads."""
+    if request.headers.get("X-Requested-With") == "XMLHttpRequest":
+        return response
+    if response.status_code < 200 or response.status_code >= 300:
+        return response
+    content_type = response.get("Content-Type", "")
+    if "text/html" not in content_type:
+        return response
+
+    fragment_html = response.content.decode("utf-8")
+    shell_response = legacy.erp_style_dashboard(request)
+    shell_html = shell_response.content.decode("utf-8")
+    match = re.search(r'(<main class="content">)(.*?)(</main>)', shell_html, re.S)
+    if not match:
+        return response
+    wrapped = shell_html[:match.start(2)] + fragment_html + shell_html[match.end(2):]
+    return HttpResponse(wrapped, content_type="text/html; charset=utf-8")
+
+
+def _shell(view):
+    def wrapped(request, *args, **kwargs):
+        return _wrap_control_response(request, view(request, *args, **kwargs))
+
+    wrapped.__name__ = getattr(view, "__name__", "control_view")
+    wrapped.__doc__ = getattr(view, "__doc__", None)
+    return wrapped
+
+
+control_products = _shell(legacy.control_products)
+control_stores = _shell(legacy.control_stores)
+control_categories = _shell(core.control_categories)
+control_inventory = _shell(core.control_inventory)
+control_orders = _shell(core.control_orders)
+order_detail = _shell(core.order_detail)
+store_detail = _shell(core.store_detail)
+control_applications = _shell(core.control_applications)
+control_payments = _shell(core.control_payments)
+control_finance = _shell(core.control_finance)
+control_variants = _shell(core.control_variants)
+
 order_status = core.order_status
 order_customer_message = core.order_customer_message
 order_chat_message = core.order_chat_message
 order_chat_open = core.order_chat_open
-store_detail = core.store_detail
 store_design_save = core.store_design_save
 store_section_save = core.store_section_save
 store_section_delete = core.store_section_delete
@@ -115,7 +149,3 @@ store_category_save = core.store_category_save
 store_category_delete = core.store_category_delete
 store_branch_save = core.store_branch_save
 store_branch_delete = core.store_branch_delete
-control_applications = core.control_applications
-control_payments = core.control_payments
-control_finance = core.control_finance
-control_variants = core.control_variants
